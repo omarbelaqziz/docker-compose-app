87aafb8f67a2813331c2f75ef57cb1d2859d7157
omarbelaqziz <omar.belaqziz@gmail.com>
2022-11-15 15:23:05 +0100

    first commit

---
+++ frontend-ui/src/components/tutorials-list.component.js
@@ -0,0 +1,132 @@
+import React, { Component } from "react";
+import TutorialDataService from "../services/tutorial.service";
+import { Link } from "react-router-dom";
+
+export default class TutorialsList extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangeSearchTitle = this.onChangeSearchTitle.bind(this);
+    this.retrieveTutorials = this.retrieveTutorials.bind(this);
+    this.refreshList = this.refreshList.bind(this);
+    this.setActiveTutorial = this.setActiveTutorial.bind(this);
+    this.removeAllTutorials = this.removeAllTutorials.bind(this);
+    this.searchTitle = this.searchTitle.bind(this);
+
+    this.state = {
+      tutorials: [],
+      currentTutorial: null,
+      currentIndex: -1,
+      searchTitle: ""
+    };
+  }
+
+  componentDidMount() {
+    this.retrieveTutorials();
+  }
+
+  onChangeSearchTitle(e) {
+    const searchTitle = e.target.value;
+
+    this.setState({
+      searchTitle: searchTitle
+    });
+  }
+
+  retrieveTutorials() {
+    TutorialDataService.getAll()
+      .then(response => {
+        this.setState({
+          tutorials: response.data
+        });
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  refreshList() {
+    this.retrieveTutorials();
+    this.setState({
+      currentTutorial: null,
+      currentIndex: -1
+    });
+  }
+
+  setActiveTutorial(tutorial, index) {
+    this.setState({
+      currentTutorial: tutorial,
+      currentIndex: index
+    });
+  }
+
+  removeAllTutorials() {
+    TutorialDataService.deleteAll()
+      .then(response => {
+        console.log(response.data);
+        this.refreshList();
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  searchTitle() {
+    this.setState({
+      currentTutorial: null,
+      currentIndex: -1
+    });
+
+    TutorialDataService.findByTitle(this.state.searchTitle)
+      .then(response => {
+        this.setState({
+          tutorials: response.data
+        });
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  render() {
+    const { searchTitle, tutorials, currentTutorial, currentIndex } = this.state;
+
+    return (
+      <div className="list row">
+        <div className="col-md-6">
+          <h4>Tutorials List</h4>
+          <ul className="list-group">
+            {tutorials &&
+              tutorials.map((tutorial, index) => (
+                <li>
+                  <div>
+                    <h4>{tutorial.title}</h4>
+                    <div>
+                      <label>
+                        <strong>Description:</strong>
+                      </label>{" "}
+                      {tutorial.description}
+                    </div>
+                    <div>
+                      <label>
+                        <strong>Status:</strong>
+                      </label>{" "}
+                      {tutorial.published ? "Published" : "Pending"}
+                    </div>
+
+                    <Link
+                      to={"/tutorials/" + tutorial.id}
+                      className="badge badge-warning"
+                    >
+                      Edit
+                    </Link>
+                  </div>
+                </li>
+              ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
